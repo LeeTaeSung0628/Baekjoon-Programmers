004edaeafbbdf3eba9fd44a46fdc3243d847bba2
Teasung Lee <93201296+tete123456@users.noreply.github.com>
2023-06-08 03:04:24 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -5,6 +5,9 @@ input = sys.stdin.readline
 n = int(input())
 arr = list(map(int,input().split()))
 
+"""
+dp 풀이법
+
 dp = [1] * n
 
 res = []
@@ -14,5 +17,38 @@ for i in range(n):
             dp[i] = max(dp[i],dp[j]+1)
 
 
-print(max(dp))
+print("dp : ",max(dp))
+"""
+
+"""
+이진탐색
+"""
+def ezin(x,ez):
+
+    start = 0
+    end = len(ez)-1
+    mid = ((start + end) // 2) 
+    while start < end:
+        mid = ((start + end) // 2) 
+
+        if ez[mid] >= x: #중간값보다 작다면
+            end = mid
+        elif ez[mid] < x: #중간값보다 크다면
+            start = mid+1 #나머지를 버렸으므로, 스타트값이 반복될수 있다.
+        elif ez[mid] == x:
+            ez[mid] = x
+            break
+
+    ez[end] = x
+
+ez = [arr[0]]
+for i in range(1,n):
+    if ez[-1] < arr[i]:
+        ez.append(arr[i])
+    else:
+        ezin(arr[i],ez)
+
+print(len(ez))
+
+
 

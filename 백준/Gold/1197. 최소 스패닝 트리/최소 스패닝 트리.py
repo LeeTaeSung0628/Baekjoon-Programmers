d247605aafbfea9130b3f354db2653f736b17379
Teasung Lee <93201296+LeeTaeSung0628@users.noreply.github.com>
2023-08-01 22:57:39 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 4492 ms, Memory: 212004 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,52 @@
+
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int,input().split())
+edge = [list(map(int,input().split())) for _ in range(M)]
+
+#최소 스패닝 트리 ( 사이클이 발생하지 않는 최소 간선)
+# -> 유니온 파인드로 구한다( 크루스칼 알고리즘 )
+"""
+1. 간선을 작은순으로 정렬한다
+2. 현재 간선이 사이클을 발생시키는지 확인한다(같은 부모인지 find)
+2.1 발생시키지 않는다면 최소신장트리에 포함(유니온)시킨다
+2.2 발생시키면 포함시키지 않는다
+"""
+#부모찾기
+def find(parent, x):
+    #부모가 자기 자신이라면 리텃
+    if parent[x] == x:
+        return x
+    else:
+        return find(parent, parent[x])
+
+#같은 부모로 만들기
+def union(parent, x, y):
+    xp = find(parent, x)
+    yp = find(parent, y)
+
+    parent[yp] = xp
+    
+    
+parent = [[] for _ in range(N+1)]
+#모든 부모를 자기자신으로 초기화
+for i in range(1,N+1):
+    parent[i] = i
+
+#비용이 낮은 순서로 정렬
+edge = sorted(edge, key=lambda x : x[2])
+
+total = 0
+for item in edge:
+    x, y, dist = item
+    #부모가 같다면(싸이클이 생긴다)
+    if find(parent, x) == find(parent, y):
+        continue
+    else:
+        #부모가 다르면 합치고, 거리 추가
+        union(parent,x, y)
+        total += dist
+
+print(total)
